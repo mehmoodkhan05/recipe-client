7e0d151348b085d97a62ec5176f7e2e2608c98b1
Mehmood Khan <mehmadk05@gmail.com>
2024-06-03 18:17:39 +0500

    recipe ui completed some responsiveness remaining and working on it

--- js/main.js
+++ js/main.js
@@ -14,18 +14,7 @@
     $(".logo, .text-heading, .text-subHeading, .nav-text").toggleClass(
       "d-none"
     );
-    $(".sidebar_header-content").toggleClass("pt-4");
+    $(".sidebar_header-content").toggleClass("pt-4 customImg");
+    $(".components").toggleClass("customCSS");
   });
-
-//   $(document).ready(function () {
-//     const cardContainer = $("#cardContainer");
-//     const cardTemplate = $("#cardTemplate");
-
-//     for (let i = 0; i < 3; i++) {
-//       const clone = cardTemplate.clone();
-// 	  clone.removeClass('card-template');
-//       clone.css("display", "flex");
-//       cardContainer.append(clone);
-//     }
-//   });
 })(jQuery);
